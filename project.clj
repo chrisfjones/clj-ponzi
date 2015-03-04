691eca46b753789efaae424258580ff2488ab0bf
Chris Jones <chris.jones@jivesoftware.com>
2015-03-03 17:31:06 -0800

    ok, it's a functional library now

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-ponzi2 "0.1.0"
+(defproject clj-ponzi "0.1.0"
   :description "Datomic helpers"
   :url "http://github.com/chrisfjones/clj-ponzi"
   :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.datomic/datomic-free "0.9.5130"]])
+                 [com.datomic/datomic-free "0.9.5130" :exclusions [joda-time]]])
