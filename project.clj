eff6d826685decd170564267616581799b5c681f
Chris Jones <chris.f.jones@gmail.com>
2016-04-18 07:52:44 -0700

    deps

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Datomic helpers"
   :url "http://github.com/chrisfjones/clj-ponzi"
   :license {:name "MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.datomic/datomic-free "0.9.5130" :exclusions [joda-time]]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [com.datomic/datomic-free "0.9.5302" :exclusions [joda-time]]])
